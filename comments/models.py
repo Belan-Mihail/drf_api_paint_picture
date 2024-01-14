f0f11f9f6ebdd9081e0f4ab3fe9114fe8093772c
Belan-Mihail Belan <belan_mihail@inbox.ru>
2024-01-14 21:55:53 +0000

    create cpmments model and migrate it to DB

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from pictures.models import Picture
 
-# Create your models here.
+
+class Comment(models.Model):
+    """
+    Comment model, related to User and Picture
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    picture = models.ForeignKey(Picture, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.content
