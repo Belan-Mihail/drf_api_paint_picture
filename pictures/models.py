95f6bf22820242fd46c3465793640291a212a8f0
Belan-Mihail Belan <belan_mihail@inbox.ru>
2024-01-14 20:56:12 +0000

    create picture model and migrate it to DB

--- pictures/models.py
+++ pictures/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Picture(models.Model):
+    """
+    Picture model for post-pictures content
+    """
+    
+    category_choices = [
+            ('landscapes', 'landscapes'),
+            ('animals', 'animals'),
+            ('plants', 'plants'),
+            ('abstraction', 'abstraction'),
+            ('other', 'other'),
+        ]
+
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    image = models.ImageField(
+        upload_to='images/', default='../default_post_jtlmh6', blank=True
+    )
+    picture_category = models.CharField(
+        max_length=32, choices=category_choices, default='other'
+    )
+
+
+    class Meta:
+        ordering = ['-created_at']
+
+
+    def __str__(self):
+        return f'{self.id} {self.title}'
