76cdadcbbbd37ebe6c99ff986917fe6be09070c5
Belan-Mihail Belan <belan_mihail@inbox.ru>
2024-01-14 21:28:21 +0000

    add PictureList view and PictureDetail view for Picture model

--- pictures/views.py
+++ pictures/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import permissions, generics
+from .models import Picture
+from .serializers import PictureSerializer
+from drf_paintpicture.permissions import IsOwnerOrReadOnly
 
-# Create your views here.
+    
+class PictureList(generics.ListCreateAPIView):
+    """
+    List pictures or create a picture if logged in
+    """
+    serializer_class = PictureSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Picture.objects.all().order_by('-created_at')
+
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class PictureDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve a picture and edit or delete it if you own it.
+    """
+    serializer_class = PictureSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Picture.objects.all().order_by('-created_at')
