192afa6990b31209f8611c510156a5bd8a432a5d
Belan-Mihail Belan <belan_mihail@inbox.ru>
2024-01-15 20:19:03 +0000

    create likes model and migrate it to DB

--- likes/models.py
+++ likes/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from pictures.models import Picture
 
-# Create your models here.
+
+class Likes(models.Model):
+    """
+    Likes model, related to User and Picture
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    picture = models.ForeignKey(Picture, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    class Meta:
+        ordering = ['-created_at']
+        # Sets of field names that, taken together, must be unique
+        unique_together = ['owner', 'picture']
+
+    def __str__(self):
+        return f'{self.owner} & {self.picture}'
