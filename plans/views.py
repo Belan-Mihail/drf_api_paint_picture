523e40c7173a4c4db0f1a801076f25f41960d861
Belan-Mihail Belan <belan_mihail@inbox.ru>
2024-01-15 22:30:46 +0000

    create PlanList view and PlanDetail view for plan model

--- plans/views.py
+++ plans/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from drf_paintpicture.permissions import IsOwnerOrReadOnly
+from .models import Plan
+from .serializers import PlanSerializer
 
-# Create your views here.
+
+class PlanList(generics.ListCreateAPIView):
+    """
+    List Plan or create plan.
+    """
+
+    serializer_class = PlanSerializer
+    permission_classes = [IsOwnerOrReadOnly] 
+    queryset = Plan.objects.all()
+
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class PlanDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve a Plan, or update or delete it by id if you own it.
+    """
+    permission_classes = [IsOwnerOrReadOnly] 
+    serializer_class = PlanSerializer
+    queryset = Plan.objects.all()
