5a4a2d6ad4c3e21414c390b76cec72d379e9fb2e
Belan-Mihail Belan <belan_mihail@inbox.ru>
2024-01-15 20:22:21 +0000

    create serializers for likes model

---
+++ likes/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from django.db import IntegrityError
+from .models import Likes
+
+
+class LikesSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Like model
+    The create method handles the unique constraint on 'owner' and 'picture'
+    """
+
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+
+    class Meta:
+        model = Likes
+
+        fields = [
+            'id',  'owner', 'picture', 'created_at'
+        ]
+    
+
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'possible duplicate'
+            })
+
