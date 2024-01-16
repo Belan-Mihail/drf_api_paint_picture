ad2304be948dc701b0bcce56fe5ad946e7516f10
Belan-Mihail Belan <belan_mihail@inbox.ru>
2024-01-16 14:41:29 +0000

    edit profiles viewa. add filters and annotate to querysets

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,6 @@
-from rest_framework import permissions, generics
+from django.db.models import Count
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import permissions, generics, filters
 from .models import Profile
 from .serializers import ProfileSerializer
 from drf_paintpicture.permissions import IsOwnerOrReadOnly
@@ -7,8 +9,27 @@ class ProfileList(generics.ListCreateAPIView):
     """
     List all profiles
     """
+    queryset = Profile.objects.annotate(
+        pictures_count=Count('owner__picture', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
     serializer_class = ProfileSerializer
-    queryset = Profile.objects.all().order_by('-created_at')
+    filter_backends = [
+        filters.OrderingFilter,
+        DjangoFilterBackend,
+    ]
+    filterset_fields = [
+        'owner__following__followed__profile',
+        'owner__followed__owner__profile',
+    ]
+    ordering_fields = [
+        'pictures_count',
+        'followers_count',
+        'following_count',
+        'owner__following__created_at',
+        'owner__followed__created_at',
+    ]
 
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
@@ -17,4 +38,8 @@ class ProfileDetail(generics.RetrieveUpdateAPIView):
     """
     permission_classes = [IsOwnerOrReadOnly]
     serializer_class = ProfileSerializer
-    queryset = Profile.objects.all().order_by('-created_at')
+    queryset = Profile.objects.annotate(
+        pictures_count=Count('owner__picture', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
