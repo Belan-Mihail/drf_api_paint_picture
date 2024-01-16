d6b4c07c177e0732170e262bb996a8e424d970c2
Belan-Mihail Belan <belan_mihail@inbox.ru>
2024-01-16 20:38:09 +0000

    edit pictures serilizers: add additional fields, get like method

--- pictures/serializers.py
+++ pictures/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Picture
+from likes.models import Likes
 
 
 class PictureSerializer(serializers.ModelSerializer):
@@ -11,6 +12,9 @@ class PictureSerializer(serializers.ModelSerializer):
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
     profile_greeting = serializers.ReadOnlyField(source='owner.profile.greeting')
+    like_id = serializers.SerializerMethodField()
+    likes_count = serializers.ReadOnlyField()
+    comments_count = serializers.ReadOnlyField()
 
 
     def validate_image(self, value):
@@ -25,6 +29,15 @@ class PictureSerializer(serializers.ModelSerializer):
                 'Image width larger than 4096px!'
             )
         return value
+    
+    def get_like_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            like = Like.objects.filter(
+                owner=user, picture=obj
+            ).first()
+            return like.id if like else None
+        return None
 
 
     def validate_description(self, value):
@@ -41,5 +54,6 @@ class PictureSerializer(serializers.ModelSerializer):
 
         fields = [
             'id', 'owner', 'profile_id', 'profile_image', 'created_at', 'updated_at', 'title',
-            'description', 'image', 'picture_category', 'is_owner', 'profile_greeting', 
+            'description', 'image', 'picture_category', 'is_owner', 'profile_greeting',
+            'like_id', 'likes_count', 'comments_count', 
         ]
