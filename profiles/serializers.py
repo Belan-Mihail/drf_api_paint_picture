6fa18110aef01b1ed84179910f36b9a1357fbb20
Belan-Mihail Belan <belan_mihail@inbox.ru>
2024-01-16 14:23:46 +0000

    edit ProfileSerializer add to it extra fields and method get_following_id

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import Profile
-
+from followers.models import Followers
 
 
 class ProfileSerializer(serializers.ModelSerializer):
@@ -9,6 +9,10 @@ class ProfileSerializer(serializers.ModelSerializer):
     """
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    following_id = serializers.SerializerMethodField()
+    pictures_count = serializers.ReadOnlyField()
+    followers_count = serializers.ReadOnlyField()
+    following_count = serializers.ReadOnlyField()
 
 
     def get_is_owner(self, obj):
@@ -21,11 +25,22 @@ class ProfileSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError('Content must contain at least 3 characters')
         return value
     
+    
+    def get_following_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            following = Followers.objects.filter(
+                owner=user, followed=obj.owner
+            ).first()
+            return following.id if following else None
+        return None
+    
 
     class Meta:
         model = Profile
 
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'name',
-            'content', 'image', 'greeting', 'is_owner'
+            'content', 'image', 'greeting', 'is_owner', 'following_id', 'pictures_count',
+            'followers_count', 'following_count',
         ]
