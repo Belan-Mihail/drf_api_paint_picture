bb892d6a2885f76ccb2ac71fdaa8dd581260c23c
Belan-Mihail Belan <belan_mihail@inbox.ru>
2024-02-02 16:37:44 +0000

    create test (user_can_list_pictures)

--- pictures/tests.py
+++ pictures/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Picture
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+class PictureListViewTests(APITestCase):
+    # setUp before all tests
+    def setUp(self):
+        User.objects.create_user(username='user1', password='pass')
+    
+    
+    def test_can_list_pictures(self):
+        user = User.objects.get(username='user1')
+        obj = Picture.objects.create(owner=user, title='a test title')
+        response = self.client.get('/pictures/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(obj.title, 'a test title')
+        self.assertTrue(isinstance(obj, Picture))
