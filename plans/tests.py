85b9a91e3c71faef70c6b00a34d4a2d149513f9d
Belan-Mihail Belan <belan_mihail@inbox.ru>
2024-02-02 19:43:26 +0000

    create PlanListViewTests and test_can_list_plans

--- plans/tests.py
+++ plans/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Plan
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+class PlanListViewTests(APITestCase):
+    # setUp before all tests
+    def setUp(self):
+        User.objects.create_user(username='user1', password='pass')
+    
+    
+    def test_can_list_plans(self):
+        user = User.objects.get(username='user1')
+        obj = Plan.objects.create(
+            owner=user, plans_title='a test title',
+            plans_description='a plans_description',
+            plans_date='2024-08-14',
+            until='False'
+        )
+        response = self.client.get('/plans/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(obj.plans_title, 'a test title')
+        self.assertEqual(obj.plans_description, 'a plans_description')
+        self.assertTrue(isinstance(obj, Plan))
