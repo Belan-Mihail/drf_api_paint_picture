84e9c7885cf3ee493b781b273a339f05c2e9a11d
Belan-Mihail Belan <belan_mihail@inbox.ru>
2024-02-02 20:22:18 +0000

    add test_logged_in_user_can_create_plan, test_logged_in_user_cant_create_plan_wit_invalid_date, test_logged_in_user_cant_create_plan_wit_invalid_title_lengt and test_logged_in_user_can_create_plan_without_dafault_value

--- plans/tests.py
+++ plans/tests.py
@@ -12,7 +12,8 @@ class PlanListViewTests(APITestCase):
     def test_can_list_plans(self):
         user = User.objects.get(username='user1')
         obj = Plan.objects.create(
-            owner=user, plans_title='a test title',
+            owner=user, 
+            plans_title='a test title',
             plans_description='a plans_description',
             plans_date='2024-08-14',
             until='False'
@@ -22,3 +23,56 @@ class PlanListViewTests(APITestCase):
         self.assertEqual(obj.plans_title, 'a test title')
         self.assertEqual(obj.plans_description, 'a plans_description')
         self.assertTrue(isinstance(obj, Plan))
+    
+    
+    def test_logged_in_user_can_create_plan(self):
+        self.client.login(username='user1', password='pass')
+        response = self.client.post('/plans/', {
+            'plans_title': 'a test title',
+            'plans_description': 'plans_description',
+            'plans_date': '2024-08-14',
+            'until': 'False' 
+        })
+        count = Plan.objects.count()
+        obj = Plan.objects.get()
+        self.assertEqual(count, 1)
+        self.assertEqual(obj.plans_title, 'a test title')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    
+    
+    def test_logged_in_user_cant_create_plan_wit_invalid_date(self):
+        self.client.login(username='user1', password='pass')
+        response = self.client.post('/plans/', {
+            'plans_title': 'a test title',
+            'plans_description': 'plans_description',
+            'plans_date': '2024-13-32',
+            'until': 'False' 
+        })
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+    
+    
+    def test_logged_in_user_cant_create_plan_wit_invalid_title_lengt(self):
+        self.client.login(username='user1', password='pass')
+        response = self.client.post('/plans/', {
+            'plans_title': 'a'*300,
+            'plans_description': 'plans_description',
+            'plans_date': '2024-13-32',
+            'until': 'False' 
+        })
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        
+        
+    def test_logged_in_user_can_create_plan_without_dafault_value(self):
+        self.client.login(username='user1', password='pass')
+        response = self.client.post('/plans/', {
+            'plans_title': 'a test title',
+            'plans_description': 'plans_description',
+            'plans_date': '2024-08-14',
+             
+        })
+        obj = Plan.objects.get()
+        self.assertEqual(obj.until, False)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    
+    
+    
