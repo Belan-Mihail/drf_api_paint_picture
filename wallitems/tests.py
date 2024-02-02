e3c4f626d4ebb075dd1512c0c8a680144098edcc
Belan-Mihail Belan <belan_mihail@inbox.ru>
2024-02-02 19:18:28 +0000

    create WallItemListViewTests and test_can_list_wallitem

--- wallitems/tests.py
+++ wallitems/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import WallItem
+from profiles.models import Profile
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+class WallItemListViewTests(APITestCase):
+    # setUp before all tests
+    def setUp(self):
+        user = User.objects.create_user(username='user1', password='pass')
+        wallitem = WallItem.objects.create(owner=user, profile_id=1, message='message1')
+    
+    
+    def test_can_list_wallitem(self):
+        user = User.objects.get(username='user1')
+        obj = WallItem.objects.get(id=1)
+        response = self.client.get('/wallitems/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(obj.message, 'message1')
+        self.assertTrue(isinstance(obj, WallItem))
