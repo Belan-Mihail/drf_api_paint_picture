7758edf94f331928e733fb032d1def50c9666226
Belan-Mihail Belan <belan_mihail@inbox.ru>
2024-02-02 19:29:09 +0000

    create WallItemDetailViewTests and add test_can_retrieve_wallitems_using_valid_id

--- wallitems/tests.py
+++ wallitems/tests.py
@@ -40,3 +40,26 @@ class WallItemListViewTests(APITestCase):
     def test_user_not_logged_in_cant_create_wallitem(self):
         response = self.client.post('/wallitems/', {'profile': 1, 'message': 'I cant create'})
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class WallItemDetailViewTests(APITestCase):
+
+    def setUp(self):
+        user1 = User.objects.create_user(username='user1', password='pass')
+        user2 = User.objects.create_user(username='user2', password='pass')
+        wallitem = WallItem.objects.create(owner=user1, profile_id=1, message='message1')
+        wallitem2 = WallItem.objects.create(owner=user2, profile_id=2, message='message2')
+        
+    
+    def test_can_retrieve_comments_using_valid_id(self):
+        response = self.client.get('/wallitems/1/')
+        response2 = self.client.get('/wallitems/2/')
+
+        self.assertEqual(response.data['message'], 'message1')
+        self.assertEqual(response.data['owner'], 'user1')
+        self.assertNotEqual(response.data['owner'], 'user2')
+        self.assertEqual(response2.data['message'], 'message2')
+        self.assertEqual(response2.data['owner'], 'user2')
+        self.assertNotEqual(response2.data['owner'], 'user1')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response2.status_code, status.HTTP_200_OK)
