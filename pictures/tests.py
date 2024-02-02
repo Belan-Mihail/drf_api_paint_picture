3d047ec7330812d3ed04776f519a99589e6c947c
Belan-Mihail Belan <belan_mihail@inbox.ru>
2024-02-02 16:51:33 +0000

    Add test retrieve picture using valid id and cant retrieve picture using invalid id

--- pictures/tests.py
+++ pictures/tests.py
@@ -31,3 +31,34 @@ class PictureListViewTests(APITestCase):
     def test_user_not_logged_in_cant_create_picture(self):
         response = self.client.post('/pictures/', {'title': 'a test title2'})
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class PictureDetailViewTests(APITestCase):
+
+    def setUp(self):
+        user1 = User.objects.create_user(username='user1', password='pass')
+        user2 = User.objects.create_user(username='user2', password='pass')
+        Picture.objects.create(
+            owner=user1, title='a title1', description='description1'
+        )
+        Picture.objects.create(
+            owner=user2, title='a title2', description='description2'
+        )
+    
+    def test_can_retrieve_picture_using_valid_id(self):
+        response = self.client.get('/pictures/1/')
+        response2 = self.client.get('/pictures/2/')
+
+        self.assertEqual(response.data['title'], 'a title1')
+        self.assertEqual(response.data['owner'], 'user1')
+        self.assertNotEqual(response.data['owner'], 'user2')
+        self.assertEqual(response2.data['title'], 'a title2')
+        self.assertEqual(response2.data['owner'], 'user2')
+        self.assertNotEqual(response2.data['owner'], 'user1')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response2.status_code, status.HTTP_200_OK)
+    
+    
+    def test_cant_retrieve_picture_using_invalid_id(self):
+        response = self.client.get('/posts/999/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
