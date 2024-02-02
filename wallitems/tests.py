aba8ab54163235616a5bba9afe4c4b79e580c14d
Belan-Mihail Belan <belan_mihail@inbox.ru>
2024-02-02 19:25:18 +0000

    add test_cant_list_wallitems_using_invalid_url, add test_logged_in_user_can_create_wallitem, test_user_not_logged_in_cant_create_wallitem

--- wallitems/tests.py
+++ wallitems/tests.py
@@ -18,3 +18,25 @@ class WallItemListViewTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(obj.message, 'message1')
         self.assertTrue(isinstance(obj, WallItem))
+    
+    
+    def test_cant_list_wallitems_using_invalid_url(self):
+        response = self.client.get('/walli45tems/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+    
+    
+    def test_logged_in_user_can_create_wallitem(self):
+        self.client.login(username='user1', password='pass')
+        wallitem = WallItem.objects.get(id=1)
+        user = User.objects.get(username='user1')
+        response = self.client.post('/wallitems/', {'profile': 1, 'message': 'hello'})
+        count = WallItem.objects.count()
+        wallitem2 = WallItem.objects.get(id=2)
+        self.assertEqual(count, 2)
+        self.assertEqual(wallitem2.message, 'hello')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    
+
+    def test_user_not_logged_in_cant_create_wallitem(self):
+        response = self.client.post('/wallitems/', {'profile': 1, 'message': 'I cant create'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
