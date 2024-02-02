a8bb69432afd41aa1c621b0ac20beb080d7c2a5b
Belan-Mihail Belan <belan_mihail@inbox.ru>
2024-02-02 20:51:18 +0000

    Create PlanDetailViewTests and test_can_retrieve_plan_using_valid_id

--- plans/tests.py
+++ plans/tests.py
@@ -74,5 +74,61 @@ class PlanListViewTests(APITestCase):
         self.assertEqual(obj.until, False)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
     
+
+class PlanDetailViewTests(APITestCase):
+
+    def setUp(self):
+        user1 = User.objects.create_user(username='user1', password='pass')
+        user2 = User.objects.create_user(username='user2', password='pass')
+        Plan.objects.create(
+            owner=user1, 
+            plans_title='title1',
+            plans_description='description1',
+            plans_date='2024-08-14',
+            until='False'
+        )
+        Plan.objects.create(
+            owner=user2, 
+            plans_title='title2',
+            plans_description='description2',
+            plans_date='2024-12-03',
+            until='True'
+        )
     
+    def test_can_retrieve_plan_using_valid_id(self):
+        response = self.client.get('/plans/1/')
+        response2 = self.client.get('/plans/2/')
+
+        self.assertEqual(response.data['plans_title'], 'title1')
+        self.assertEqual(response.data['owner'], 'user1')
+        self.assertEqual(response.data['until'], False)
+        self.assertNotEqual(response.data['owner'], 'user2')
+        self.assertEqual(response2.data['plans_title'], 'title2')
+        self.assertEqual(response2.data['owner'], 'user2')
+        self.assertEqual(response2.data['until'], True)
+        self.assertNotEqual(response2.data['owner'], 'user1')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response2.status_code, status.HTTP_200_OK)
+    
+    
+    # def test_cant_retrieve_picture_using_invalid_id(self):
+    #     response = self.client.get('/posts/999/')
+    #     self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+    
+    
+    # def test_user_can_update_own_picture(self):
+    #     self.client.login(username='user1', password='pass')
+    #     response = self.client.put('/pictures/1/', {'title': 'a new title'})
+    #     picture = Picture.objects.filter(pk=1).first()
+    #     self.assertEqual(picture.title, 'a new title')
+    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
+    
+
+    # def test_user_cant_update_not_own_picture(self):
+    #     self.client.login(username='user1', password='pass')
+    #     response = self.client.put('/pictures/2/', {'title': 'a new title'})
+    #     picture = Picture.objects.filter(pk=2).first()
+    #     self.assertEqual(picture.title, 'a title2')
+    #     self.assertNotEqual(response.status_code, status.HTTP_200_OK)
+    #     self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
     
